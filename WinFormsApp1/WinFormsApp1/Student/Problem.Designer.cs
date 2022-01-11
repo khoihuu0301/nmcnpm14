8a15147be6db241b0622ffb11bc8ff3322c29149
quangminh1622 <90900929+quangminh1622@users.noreply.github.com>
2022-01-12 01:59:41 +0700

    problem

---
+++ WinFormsApp1/WinFormsApp1/Student/Problem.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace WinFormsApp1.Student
+{
+    partial class Problem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Problem));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pythonifelselbl = new System.Windows.Forms.Label();
+            this.problembtn = new System.Windows.Forms.Button();
+            this.submissionsbtn = new System.Windows.Forms.Button();
+            this.disscusionsbtn = new System.Windows.Forms.Button();
+            this.editurialbtn = new System.Windows.Forms.Button();
+            this.tutorialbtn = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.runcodebtn = new System.Windows.Forms.Button();
+            this.submitcodebtn = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1263, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pythonifelselbl);
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1263, 48);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // pythonifelselbl
+            // 
+            this.pythonifelselbl.AutoSize = true;
+            this.pythonifelselbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pythonifelselbl.Location = new System.Drawing.Point(37, 4);
+            this.pythonifelselbl.Name = "pythonifelselbl";
+            this.pythonifelselbl.Size = new System.Drawing.Size(143, 28);
+            this.pythonifelselbl.TabIndex = 0;
+            this.pythonifelselbl.Text = "Python If-Else";
+            this.pythonifelselbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // problembtn
+            // 
+            this.problembtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.problembtn.Location = new System.Drawing.Point(50, 127);
+            this.problembtn.Name = "problembtn";
+            this.problembtn.Size = new System.Drawing.Size(107, 39);
+            this.problembtn.TabIndex = 3;
+            this.problembtn.Text = "Problem";
+            this.problembtn.UseVisualStyleBackColor = true;
+            // 
+            // submissionsbtn
+            // 
+            this.submissionsbtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.submissionsbtn.Location = new System.Drawing.Point(155, 127);
+            this.submissionsbtn.Name = "submissionsbtn";
+            this.submissionsbtn.Size = new System.Drawing.Size(139, 39);
+            this.submissionsbtn.TabIndex = 4;
+            this.submissionsbtn.Text = "Submissions";
+            this.submissionsbtn.UseVisualStyleBackColor = true;
+            // 
+            // disscusionsbtn
+            // 
+            this.disscusionsbtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.disscusionsbtn.Location = new System.Drawing.Point(293, 127);
+            this.disscusionsbtn.Name = "disscusionsbtn";
+            this.disscusionsbtn.Size = new System.Drawing.Size(135, 39);
+            this.disscusionsbtn.TabIndex = 5;
+            this.disscusionsbtn.Text = "Discussions";
+            this.disscusionsbtn.UseVisualStyleBackColor = true;
+            // 
+            // editurialbtn
+            // 
+            this.editurialbtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.editurialbtn.Location = new System.Drawing.Point(424, 127);
+            this.editurialbtn.Name = "editurialbtn";
+            this.editurialbtn.Size = new System.Drawing.Size(109, 39);
+            this.editurialbtn.TabIndex = 6;
+            this.editurialbtn.Text = "Editurial";
+            this.editurialbtn.UseVisualStyleBackColor = true;
+            // 
+            // tutorialbtn
+            // 
+            this.tutorialbtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tutorialbtn.Location = new System.Drawing.Point(531, 127);
+            this.tutorialbtn.Name = "tutorialbtn";
+            this.tutorialbtn.Size = new System.Drawing.Size(108, 39);
+            this.tutorialbtn.TabIndex = 7;
+            this.tutorialbtn.Text = "Tutorial";
+            this.tutorialbtn.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(51, 190);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(596, 342);
+            this.panel3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(75, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(482, 315);
+            this.label3.TabIndex = 0;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(681, 190);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(393, 342);
+            this.panel4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(15, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 294);
+            this.label4.TabIndex = 0;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 576);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1024, 23);
+            this.textBox1.TabIndex = 11;
+            // 
+            // runcodebtn
+            // 
+            this.runcodebtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.runcodebtn.Location = new System.Drawing.Point(787, 538);
+            this.runcodebtn.Name = "runcodebtn";
+            this.runcodebtn.Size = new System.Drawing.Size(120, 32);
+            this.runcodebtn.TabIndex = 12;
+            this.runcodebtn.Text = "Run Code";
+            this.runcodebtn.UseVisualStyleBackColor = true;
+            // 
+            // submitcodebtn
+            // 
+            this.submitcodebtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.submitcodebtn.Location = new System.Drawing.Point(913, 538);
+            this.submitcodebtn.Name = "submitcodebtn";
+            this.submitcodebtn.Size = new System.Drawing.Size(161, 32);
+            this.submitcodebtn.TabIndex = 13;
+            this.submitcodebtn.Text = "Submit Code";
+            this.submitcodebtn.UseVisualStyleBackColor = true;
+            this.submitcodebtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Problem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.submitcodebtn);
+            this.Controls.Add(this.runcodebtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.tutorialbtn);
+            this.Controls.Add(this.editurialbtn);
+            this.Controls.Add(this.disscusionsbtn);
+            this.Controls.Add(this.submissionsbtn);
+            this.Controls.Add(this.problembtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Problem";
+            this.Text = "Problem";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label pythonifelselbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button problembtn;
+        private System.Windows.Forms.Button submissionsbtn;
+        private System.Windows.Forms.Button disscusionsbtn;
+        private System.Windows.Forms.Button editurialbtn;
+        private System.Windows.Forms.Button tutorialbtn;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button runcodebtn;
+        private System.Windows.Forms.Button submitcodebtn;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Student/Problem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Student
+{
+    public partial class Problem : Form
+    {
+        public Problem()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void asd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
