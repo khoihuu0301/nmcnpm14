c77e86604f0105bccdb29388641baed08ed821ce
Nguyễn Hữu Khôi <gamalaxa159@gmail.com>
2022-01-11 15:29:58 +0700

    Update Program.cs

--- WinFormsApp1/WinFormsApp1/Program.cs
+++ WinFormsApp1/WinFormsApp1/Program.cs
@@ -19,6 +19,7 @@ namespace WinFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+	//Lohope
         }
     }
 }
