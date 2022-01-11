652e09225825586eb2080a6b0a6435bb43ed5fd2
quangminh1622 <90900929+quangminh1622@users.noreply.github.com>
2022-01-11 22:22:26 +0700

    Update Program.cs

---
+++ WinFormsApp1/WinFormsApp1/I_O/Signin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.I_O
+{
+    public partial class Signin : Form
+    {
+        public Signin()
+        {
+            InitializeComponent();
+        }
+    }
+}
