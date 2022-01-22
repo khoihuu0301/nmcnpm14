79b31164d43ebd1231db2f2c3e0d6192f8c7a0e8
Nguyễn Hữu Khôi <gamalaxa159@gmail.com>
2022-01-22 17:13:07 +0700

    Created, not working btw

---
+++ WinFormsApp1/WinFormsApp1/Student/Browse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Student
+{
+    public partial class Browse : Form
+    {
+        public Browse()
+        {
+            InitializeComponent();
+        }
+    }
+}
